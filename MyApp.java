3cea85b834838b06b21aacc5a490f52b1c1d6190
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2024-10-14 14:42:59 +0900

    Add files via upload
    bingo or buu

--- MyApp.java
+++ MyApp.java
@@ -5,13 +5,41 @@
 //   }
 // }
 
+// import java.util.Scanner;
+// class MyApp {
+//   public static void main(String[] args) {
+//     System.out.print("Your name?");
+//     String name = new Scanner(System.in).next();
+    
+//     System.out.println("hello " + name);
+//     System.out.println("hello " + name +  " again!");
+//   }
+// }
+
+// import java.util.Scanner;
+
+// class MyApp {
+//   public static void main(String[] args) {
+//     System.out.print("Your guess? ");
+//     Integer guess = new Scanner(System.in).nextInt();
+    
+//     System.out.println("Your guess: " + guess);
+//   }
+// }
+
 import java.util.Scanner;
+
 class MyApp {
   public static void main(String[] args) {
-    System.out.print("Your name?");
-    String name = new Scanner(System.in).next();
-    
-    System.out.println("hello " + name);
-    System.out.println("hello " + name +  " again!");
+    Integer answer = 6;
+  
+    System.out.print("Your guess? ");
+    Integer guess = new Scanner(System.in).nextInt();
+
+    if (answer == guess){
+      System.out.println("BINGO!");
+    } else {
+      System.out.println("BOO");
+    }
   }
 }
