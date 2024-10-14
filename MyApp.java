5f5e558cc5b9943485f28c6eae19f0a624d113e6
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2024-10-14 14:36:46 +0900

    Add files via upload

--- MyApp.java
+++ MyApp.java
@@ -1,6 +1,17 @@
+// class MyApp {
+//   public static void main(String[] args) {
+//     System.out.println("hello taguchi");
+//     System.out.println("hello taguchi again!");
+//   }
+// }
+
+import java.util.Scanner;
 class MyApp {
   public static void main(String[] args) {
-    System.out.println("hello taguchi");
-    System.out.println("hello taguchi again!");
+    System.out.print("Your name?");
+    String name = new Scanner(System.in).next();
+    
+    System.out.println("hello " + name);
+    System.out.println("hello " + name +  " again!");
   }
 }
