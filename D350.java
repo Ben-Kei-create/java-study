7578b1da0f53a3a46fbdb52f39619ff9d6b6de95
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2025-08-05 00:50:55 +0000

    Update D350.java with main method

---
+++ D350.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();//2
+        int line2 = sc.nextInt();//5
+        System.out.println(line*line2);
+    }
+}
