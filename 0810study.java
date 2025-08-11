df70b9f553c4686531089c2beb1750e2213bbaf6
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2025-08-11 00:33:53 +0000

    Plus;

---
+++ 0810study.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String... args) {
+        Integer i = 5;
+        System.out.println(i);
+        // 以下にコードを追加
+        System.out.println(++i);
+        
+    }
+}
+
