60a978c7b464e35a11e3693b6ba3c2db87966927
crecat <112257840+Ben-Kei-create@users.noreply.github.com>
2025-09-11 16:32:25 +0000

    study

---
+++ 0910javaStudy.java
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <article>
+            <h1>マーガレットとお勉強</h1>
+            <h2>あいさつ</h2>
+            <p>こんにちは、霧島京子です。先日、マーガレットとプログラミングの勉強をしました。</p>
+            <h2>マーガレットとしたいこと</h2>
+            <p>JavaScriptとCSSの勉強を一緒にしていけたらと思っています。</p>
+        </article>
+    </body>
+</html>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <nav>
+            <h1>最近の投稿</h1>
+            <ul>
+                <li><a href="#">ホーム</a></li>
+                <li><a href="#">プログラミング</a></li>
+                <li><a href="#">プロフィール</a></li>
+            </ul>
+        </nav>
+        <article>
+            <h1>マーガレットとお勉強</h1>
+            <h2>あいさつ</h2>
+            <p>こんにちは、霧島京子です。先日、マーガレットとプログラミングの勉強をしました。</p>
+            <h2>マーガレットとしたいこと</h2>
+            <p>JavaScriptとCSSの勉強を一緒にしていけたらと思っています。</p>
+        </article>
+    </body>
+</html>
+
